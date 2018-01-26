88c1f8e7096dd34f4c359107c0a099133317bddd
Alexander Mechler <axm8289@rit.edu>
2018-01-26 09:50:41 -0500

    Added in save functionality

--- app/components/dungeon-loot.js
+++ app/components/dungeon-loot.js
@@ -3,7 +3,20 @@ import Component from '@ember/component';
 export default Component.extend({
 	actions:{
 		save(cyan, blue, red, orange, white){
-			
+			let storage = window.localStorage;
+   			for (let i = 0, j = arguments.length; i < j; i++){
+   				if(typeof(arguments[i]) !== 'undefined' && arguments[i] != null){
+   					arguments[i].forEach(function(i){
+						if(storage.getItem(i) === null){
+							storage.setItem(i, 1);
+						}else{
+							let val = parseInt(storage.getItem(i)) + 1;
+							storage.setItem(i, val);
+						}
+					});
+				}
+			}
+			Ember.getOwner(this).lookup('router:main').transitionTo('dungeon');
 		},
 		cancel(){
 			Ember.getOwner(this).lookup('router:main').transitionTo('dungeon');
