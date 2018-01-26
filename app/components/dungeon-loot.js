35b6e0c63414d276d3388e35a475f9cb81cdbefb
Alexander Mechler <axm8289@rit.edu>
2018-01-25 22:44:38 -0500

    Added bootstrap layout for dungeon-loot

--- app/components/dungeon-loot.js
+++ app/components/dungeon-loot.js
@@ -3,9 +3,10 @@ import Component from '@ember/component';
 export default Component.extend({
 	actions:{
 		save(cyan, blue, red, orange, white){
-			cyan.forEach(function(i){
-				alert(i);
-			});
+			
+		},
+		cancel(){
+			
 		}
 	}
 });
