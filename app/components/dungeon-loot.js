c2da0f66fea13c8e7111b501313a11e375d48f60
Alexander Mechler <axm8289@rit.edu>
2018-01-25 19:29:28 -0500

    Updated dungeon-loot template

--- app/components/dungeon-loot.js
+++ app/components/dungeon-loot.js
@@ -1,4 +1,26 @@
 import Component from '@ember/component';
 
+let itemsObtained = new Set();
+
 export default Component.extend({
+	actions:{
+		generateValue(item){
+			return item;
+		},
+		alert(value){
+			alert(value);
+		},
+		toggleItem(item){
+			if(itemsObtained.has(item)){
+				itemsObtained.delete(item);
+			}else{
+				itemsObtained.add(item);
+			}
+		},
+		save(){
+			itemsObtained.forEach(function(i){
+				alert(i);
+			});
+		}
+	}
 });
